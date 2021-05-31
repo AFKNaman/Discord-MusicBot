65847ab569844b18dbf0f3968053a1e78a1deb46
Naman <75615744+AFKNaman@users.noreply.github.com>
2021-05-31 21:01:22 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,15 @@
+const activities_list = [
+    "QUERY", 
+    "SUGGESTIONS",
+    "COMPLAINS", 
+    "MUSIC"
+    "BFA"
+    ];
+
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
  },
 };
-
 
 
 
