42326287b30c81e2c2b26669ba909bd0642440ef
Naman <75615744+AFKNaman@users.noreply.github.com>
2021-05-31 21:07:58 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,8 +4,3 @@ module.exports = async (client) => {
     type: "LISTENING",//can be , WATCHING, PLAYING, STREAMING
   });
 };
-setInterval(() => {
-        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-       await client.user.setActivity(activities_list[index], { type: 'LISTENING' }); // sets bot's activities to one of the phrases in the arraylist.
-    }, 10000); // Runs this every 10 seconds.
-});
