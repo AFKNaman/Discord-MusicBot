94f370ace2bd4a917aed7cc51931a3231bc7a437
Naman <75615744+AFKNaman@users.noreply.github.com>
2021-06-12 20:55:12 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("Music", {
+  await client.user.setActivity("MUSIC", {
     type: "LISTENING",//can be , WATCHING, PLAYING, STREAMING
   });
 };
